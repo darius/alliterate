bc49b0287435efa04855db4e467f49c5fff9e078
Darius Bacon <darius@wry.me>
2013-10-26 15:09:31 +0200

    strip out punctuation

--- scoring.py
+++ scoring.py
@@ -1,7 +1,9 @@
 from __future__ import division
 from math import log
-import pronounce
+import re
 from collections import Counter
+
+import pronounce
 
 def log2(x): return log(x) / log(2)
 
@@ -22,18 +24,26 @@ def score_em_all(filename):
     with open(filename) as f:
         for line in f:
             line = line.rstrip('\n')
+            words = get_words(line)
             try:
-                nonalliterativeness = score(line.split())
+                nonalliterativeness = score(words)
                 if nonalliterativeness < infinity:
                     print nonalliterativeness, line
             except KeyError:
                 pass
 
+def get_words(text):
+    "Return text's words in order."
+    return [word.strip("'") for word in re.findall(r"['\w]+", text)]
+
 if __name__ == '__main__':
     score_em_all('great-expectations.txt')
 
-#    print key, count, count/total
 ## score('when in the course of human events'.split())
-#. 2.5216406363433186
+#. 0.3602343766204741
 ## score('peter piper picked a peck'.split())
-#. 0.7219280948873623
+#. 0.14438561897747246
+## score('peter piper picked a really random rule'.split())
+#. 0.20697366224645494
+## score('peter piper picked a different stupid zoo'.split())
+#. 0.30401218269877067
