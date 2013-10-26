1ec12fe7f24b1b88f17a922b45bce2f5e9719152
Darius Bacon <darius@wry.me>
2013-10-26 15:58:16 +0200

    use nltk tokenize, try silly variations on the formula

--- scoring.py
+++ scoring.py
@@ -2,6 +2,7 @@ from __future__ import division
 from math import log
 import re
 from collections import Counter
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 import pronounce
 
@@ -13,23 +14,35 @@ def score(phrase):
     "Lower scores mean more alliteration. But it's really super-crude."
     if not phrase: return infinity
     blah = [pronounce.phonetic(word).split() for word in phrase]
-    goober = [phones[0] for phones in blah]
+    goober = [first_consonant(phones) for phones in blah]
     counter = Counter(goober)
     total = sum(counter.values())
     entropy = sum(-count/total * log2(count/total)
                   for key, count in counter.items())
-    return entropy / len(phrase)
+#    return entropy / len(phrase), counter
+    return entropy, counter
+
+def first_consonant(phones):
+#    return phones[0]
+    for phone in phones:
+        if not pronounce.is_vowel(phone):
+            return phone
+    return ''
 
 def score_em_all(filename):
     with open(filename) as f:
-        for line in f:
-            line = line.rstrip('\n')
+        text = f.read()
+        for line in sent_tokenize(text):
+            line = line.replace('\r', ' ').replace('\n', ' ')
+#            import sys
             words = get_words(line)
+#            sys.stderr.write(repr(words) + '\n')
             try:
-                nonalliterativeness = score(words)
+                nonalliterativeness, counter= score(words)
                 if nonalliterativeness < infinity:
-                    print nonalliterativeness, line
+                    print nonalliterativeness, counter, line
             except KeyError:
+#                print 'oops', words
                 pass
 
 def get_words(text):
@@ -40,10 +53,10 @@ if __name__ == '__main__':
     score_em_all('great-expectations.txt')
 
 ## score('when in the course of human events'.split())
-#. 0.3602343766204741
+#. (2.5216406363433186, Counter({'IH0': 2, 'DH': 1, 'K': 1, 'HH': 1, 'AH1': 1, 'W': 1}))
 ## score('peter piper picked a peck'.split())
-#. 0.14438561897747246
+#. (0.7219280948873623, Counter({'P': 4, 'AH0': 1}))
 ## score('peter piper picked a really random rule'.split())
-#. 0.20697366224645494
+#. (1.4488156357251847, Counter({'P': 3, 'R': 3, 'AH0': 1}))
 ## score('peter piper picked a different stupid zoo'.split())
-#. 0.30401218269877067
+#. (2.128085278891395, Counter({'P': 3, 'S': 1, 'AH0': 1, 'Z': 1, 'D': 1}))
