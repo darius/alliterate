4f56986bc4e2d491c6ae9843153f3e5d371bb533
Darius Bacon <darius@wry.me>
2013-10-26 16:41:39 +0200

    code cleanup

--- scoring.py
+++ scoring.py
@@ -13,14 +13,10 @@ infinity = float('Inf')
 def score(phrase):
     "Lower scores mean more alliteration. But it's really super-crude."
     if not phrase: return infinity
-    blah = [pronounce.phonetic(word).split() for word in phrase]
-    goober = [first_consonant(phones) for phones in blah]
-    counter = Counter(goober)
-    total = sum(counter.values())
-    entropy = sum(-count/total * log2(count/total)
-                  for key, count in counter.items())
-#    return entropy / len(phrase), counter
-    return entropy, counter
+    word_phones = [pronounce.phonetic(word).split() for word in phrase]
+    counter = Counter(map(first_consonant, word_phones))
+#    return entropy(counter) / len(phrase), counter
+    return entropy(counter), counter
 
 def first_consonant(phones):
 #    return phones[0]
@@ -29,21 +25,26 @@ def first_consonant(phones):
             return phone
     return ''
 
+def entropy(counter):
+    total = sum(counter.values())
+    if 0 == total: return 0
+    return sum(-count/total * log2(count/total)
+               for count in counter.values())
+
 def score_em_all(filename):
     with open(filename) as f:
         text = f.read()
-        for line in sent_tokenize(text):
-            line = line.replace('\r', ' ').replace('\n', ' ')
-#            import sys
-            words = get_words(line)
-#            sys.stderr.write(repr(words) + '\n')
+        for sentence in sent_tokenize(text):
+            sentence = ' '.join(sentence.splitlines())
+            words = get_words(sentence)
             try:
-                nonalliterativeness, counter= score(words)
-                if nonalliterativeness < infinity:
-                    print nonalliterativeness, counter, line
+                nonalliterativeness, counter = score(words)
             except KeyError:
 #                print 'oops', words
                 pass
+            else:
+                if nonalliterativeness < infinity:
+                    print nonalliterativeness, counter, sentence
 
 def get_words(text):
     "Return text's words in order."
@@ -53,10 +54,10 @@ if __name__ == '__main__':
     score_em_all('great-expectations.txt')
 
 ## score('when in the course of human events'.split())
-#. (2.5216406363433186, Counter({'IH0': 2, 'DH': 1, 'K': 1, 'HH': 1, 'AH1': 1, 'W': 1}))
+#. (2.5216406363433186, Counter({'V': 2, 'DH': 1, 'K': 1, 'N': 1, 'HH': 1, 'W': 1}))
 ## score('peter piper picked a peck'.split())
-#. (0.7219280948873623, Counter({'P': 4, 'AH0': 1}))
+#. (0.7219280948873623, Counter({'P': 4, '': 1}))
 ## score('peter piper picked a really random rule'.split())
-#. (1.4488156357251847, Counter({'P': 3, 'R': 3, 'AH0': 1}))
+#. (1.4488156357251847, Counter({'P': 3, 'R': 3, '': 1}))
 ## score('peter piper picked a different stupid zoo'.split())
-#. (2.128085278891395, Counter({'P': 3, 'S': 1, 'AH0': 1, 'Z': 1, 'D': 1}))
+#. (2.128085278891395, Counter({'P': 3, '': 1, 'S': 1, 'Z': 1, 'D': 1}))
